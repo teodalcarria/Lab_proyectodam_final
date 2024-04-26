1a0bed3b4aa773e98df8b40801f8792049a9dce2
Tomas Escudero <teodalcarria@gmail.com>
2024-04-26 16:43:29 +0200

    user2 puestos los casos de prueba en TestCalculadora

--- src/testing/TestCalculadora.java
+++ src/testing/TestCalculadora.java
@@ -1,5 +1,22 @@
 package testing;
 
+import dao.CalculadoraDao;
+import dao.CalculadoraDaoImpl;
+
 public class TestCalculadora {
+	
+	public static void main(String[] args) {
+		CalculadoraDao cdao = new CalculadoraDaoImpl();
+		
+		System.out.println(cdao.sumar(20, 5));
+		System.out.println(cdao.restar(20, 5));
+		System.out.println(cdao.restar(5, 20));
+		System.out.println(cdao.multiplicar(20, 5));
+		System.out.println(cdao.dividir(20, 5));
+		System.out.println(cdao.dividir(5, 20));
+		System.out.println(cdao.resto(20, 3));
+		System.out.println(cdao.resto(7, 14));
+		
+	}
 
 }
